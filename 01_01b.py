811358619e2dc1330f6b3be80387aab6fca3dece
skhan17 <68302097+skhan17@users.noreply.github.com>
2023-02-03 19:03:04 +0000

    paste output

--- 01_01b.py
+++ 01_01b.py
@@ -24,3 +24,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# print following:
+# Counter({'of': 7, 'and': 7, 'the': 6, 'their': 6, 'nadia’s': 4, 'restaurant': 4, 'to': 4, 'garden': 3, 'nadia': 3, 'with': 3, 'a': 3, 'in': 3, 'are': 3, 'timothy': 2, 'cuisine': 2, 'from': 2, 'his': 2, 'family': 2, 'dining': 2, 'for': 2, 'be': 2, 'community': 2, 'more': 2, 'is': 1, 'creation': 1, 'husband': 1, 'wife': 1, 'team': 1, 'arbore': 1, 'americaninfused': 1, 'italianbased': 1, 'organically': 1, 'created': 1, 'was': 1, 'inspired': 1, 'by': 1, 'papa': 1, 'an': 1, 'immigrant': 1, 'italy': 1, 'who': 1, 'shared': 1, 'love': 1, 'cooking': 1, 'as': 1, 'young': 1, 'girl': 1, 'focus': 1, 'on': 1, 'using': 1, 'fresh': 1, 'ingredients': 1, 'style': 1, 'were': 1,
+#         'inspiration': 1, 'located': 1, 'heart': 1, 'main': 1, 'streets': 1, 'historic': 1, 'district': 1, 'they': 1, 'proud': 1, 'part': 1, 'rich': 1, '2011': 1, 'duo': 1, 'remodeled': 1, 'updated': 1, 'menu': 1, 'include': 1, 'newer': 1, 'diverse': 1, 'entrées': 1, 'that': 1, 'could': 1, 'made': 1, 'local': 1, 'organic': 1, 'suppliers': 1, 'preservation': 1, 'building’s': 1, 'original': 1, 'layout': 1, 'has': 1, 'allowed': 1, 'them': 1, 'create': 1, 'smaller': 1, 'intimate': 1, 'spaces': 1, 'committed': 1, 'sharing': 1, 'history': 1, 'along': 1, 'new': 1, 'inspirations': 1, 'customers': 1, 'passion': 1, 'entertainment': 1, 'hospitality': 1, 'found': 1, 'every': 1, 'aspect': 1})
