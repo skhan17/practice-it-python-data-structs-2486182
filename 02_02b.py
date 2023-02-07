dacb79c3195e5b942a4583dd01d2f5b67ca0834e
skhan17 <68302097+skhan17@users.noreply.github.com>
2023-02-07 18:11:10 +0000

    update 2.2

--- 02_02b.py
+++ 02_02b.py
@@ -1,7 +1,20 @@
+# palindromes practice
+from collections import deque
+
+def check_palindrome(word):
+    # define deque
+    d = deque(word)
+    while len(d)>1:
+        # if first and last letter do not match, its not a palindrome
+        if d.pop() != d.popleft():
+            return False
+    return True
 
 def main():
     #add code here
-    return
+    # word = "choice"
+    word = "tacocat"
+    print(check_palindrome(word))
 
 if __name__ == "__main__":
     main()
