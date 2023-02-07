d2d7bcb7132c50646a092cfc2a22c574d612f88f
skhan17 <68302097+skhan17@users.noreply.github.com>
2023-02-07 19:13:14 +0000

    attempt exercise

--- 03_02b.py
+++ 03_02b.py
@@ -1,9 +1,28 @@
+from csv import reader
+from collections import namedtuple
+
 
 def main():
-    #add code here
-    #read data/Customer.csv
-    #Create workable objects with each line
+    # add code here
+    # read data/Customer.csv
+    lastName = []
+
+    with open("data/Customer.csv", "r") as open_csv:
+        read = reader(open_csv)
+        Person = namedtuple("Person", next(read))
+        for line in read:
+            person = Person(*line)
+            print(person)
+            lastName.append(person.LastName)
+            # if person.State == "PA":
+            #     print(person)
+
+    # Create workable objects with each line
+    for l in lastName:
+        print(l)
+
     return
+
 
 if __name__ == "__main__":
     main()
