81d3dbb334488866a62734b29e9002bef57b22c7
skhan17 <68302097+skhan17@users.noreply.github.com>
2023-02-07 18:05:12 +0000

    update excercise 2.1

--- 02_01b.py
+++ 02_01b.py
@@ -1,6 +1,17 @@
+from collections import deque
 
 def main():
-    #add code here
+    # add code here
+    foods = deque(maxlen=5)
+    foods.append("STA001")
+    ordered_food = ["STA003", "SAL002", "ENT004", "DES003"]
+    deque(['STA003', 'SAL002', 'ENT004', 'DES003', 'DES002'], maxlen=5)
+    foods.extend(ordered_food)
+    foods.append("DES002")
+    # deque(['STA003', 'SAL002', 'ENT004', 'DES003', 'DES002'], maxlen=5)
+    # foods.appendleft("ENT004")
+    # deque(['ENT004', 'STA003', 'SAL002', 'ENT004', 'DES003'], maxlen=5)
+    print(foods)
     return
 
 if __name__ == "__main__":
